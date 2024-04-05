b2301885a6f39a897f732afb6b36a5b60b845cdf
Rex Hart <75512757+rexapcs@users.noreply.github.com>
2024-04-05 11:12:18 +0200

    ZombieWalkingAnimation

--- Assets/Scripts_AB/Zombie/ZombieController.cs
+++ Assets/Scripts_AB/Zombie/ZombieController.cs
@@ -1,6 +1,9 @@
 using System.Collections;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
+using System.Collections.Generic;
+
 
 public class ZombieController : MonoBehaviour
 {
@@ -9,16 +12,24 @@ public class ZombieController : MonoBehaviour
     public int damage = 100;
 
     private Rigidbody2D rb;
-    private Animator enemyAnimator;
+    private Animator animator;
     private Transform playerTransform; // Reference to the player's transform
     private bool dead = false;
     private Vector2 stop = new Vector2(0, 0);
     private bool right = true;
 
+    float absHorizontalMove = 0f;
+    float absVerticalMove = 0f;
+
+    bool isHorizontal = false;
+    bool isDown = true;
+
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        enemyAnimator = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform; // Find the player by tag
     }
 
@@ -28,13 +39,16 @@ public class ZombieController : MonoBehaviour
         if (!dead)
         {
             FollowPlayer();
+            
         }
     }
 
     private void FollowPlayer()
     {
         // Move towards the player
+        Vector2 direction = (playerTransform.position - transform.position).normalized;
         transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, speed * Time.deltaTime);
+        Movement(direction);
 
         // Flip the zombie to face the player
         if (playerTransform.position.x > transform.position.x && !right || playerTransform.position.x < transform.position.x && right)
@@ -58,6 +72,37 @@ public class ZombieController : MonoBehaviour
         }
     }
 
+    private void Movement(Vector2 direction)
+    {
+        // Use the direction to determine if the zombie is moving down
+        isDown = direction.y < 0;
+        absHorizontalMove = Mathf.Abs(direction.x);
+        absVerticalMove = Mathf.Abs(direction.y);
+
+        if (absHorizontalMove > absVerticalMove)
+        {
+            isHorizontal = true;
+        }
+        else
+        {
+            isHorizontal = false;
+        }
+
+        isDown = direction.y < 0 && !isHorizontal;
+
+        MovementAnimation();
+    }
+
+
+
+
+
+    private void MovementAnimation()
+    {
+       
+        animator.SetBool("IsDown", isDown);
+        animator.SetBool("IsHorizontal", isHorizontal);
+    }
     public void isDead()
     {
         if (dead)
@@ -70,7 +115,7 @@ public class ZombieController : MonoBehaviour
     private void DieAnimation()
     {
         rb.velocity = stop;
-        enemyAnimator.SetBool("isDead", true);
+        animator.SetBool("isDead", true);
     }
 
     void Die()
