3f79c79db47c4f8718e1cc41ceb48b313fc64f53
adamblumoff <ablumoff25@amherst.edu>
2024-04-07 23:24:16 +0200

    player attacking

--- Assets/Scripts_AB/Zombie/ZombieController.cs
+++ Assets/Scripts_AB/Zombie/ZombieController.cs
@@ -1,6 +1,11 @@
 using System.Collections;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using System;
+
 
 public class ZombieController : MonoBehaviour
 {
@@ -9,16 +14,24 @@ public class ZombieController : MonoBehaviour
     public int damage = 100;
 
     private Rigidbody2D rb;
-    private Animator enemyAnimator;
+    private Animator animator;
     private Transform playerTransform; // Reference to the player's transform
     private bool dead = false;
     private Vector2 stop = new Vector2(0, 0);
     private bool right = true;
 
+    float absHorizontalMove = 0f;
+    float absVerticalMove = 0f;
+
+    bool isHorizontal = false;
+    bool isDown = true;
+
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        enemyAnimator = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform; // Find the player by tag
     }
 
@@ -28,13 +41,28 @@ public class ZombieController : MonoBehaviour
         if (!dead)
         {
             FollowPlayer();
+            
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Check if the collision is with the player
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Call a method on the player to apply damage
+            CharacterController2D player = collision.gameObject.GetComponent<CharacterController2D>();
+            player.TakeDamage(damage);
+            Debug.Log("Damage taken");
         }
     }
 
     private void FollowPlayer()
     {
         // Move towards the player
+        Vector2 direction = (playerTransform.position - transform.position).normalized;
         transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, speed * Time.deltaTime);
+        Movement(direction);
 
         // Flip the zombie to face the player
         if (playerTransform.position.x > transform.position.x && !right || playerTransform.position.x < transform.position.x && right)
@@ -51,13 +79,44 @@ public class ZombieController : MonoBehaviour
 
     public void TakeDamage(int damage)
     {
-        health -= damage;
+        //health -= damage;
         if (health <= 0)
         {
             dead = true;
         }
     }
 
+    private void Movement(Vector2 direction)
+    {
+        // Use the direction to determine if the zombie is moving down
+        isDown = direction.y < 0;
+        absHorizontalMove = Mathf.Abs(direction.x);
+        absVerticalMove = Mathf.Abs(direction.y);
+
+        if (absHorizontalMove > absVerticalMove)
+        {
+            isHorizontal = true;
+        }
+        else
+        {
+            isHorizontal = false;
+        }
+
+        isDown = direction.y < 0 && !isHorizontal;
+
+        MovementAnimation();
+    }
+
+
+
+
+
+    private void MovementAnimation()
+    {
+       
+        animator.SetBool("IsDown", isDown);
+        animator.SetBool("IsHorizontal", isHorizontal);
+    }
     public void isDead()
     {
         if (dead)
@@ -71,7 +130,7 @@ public class ZombieController : MonoBehaviour
     private void DieAnimation()
     {
         rb.velocity = stop;
-        enemyAnimator.SetBool("isDead", true);
+        animator.SetBool("isDead", true);
     }
 
     void Die()
