2cc9dd1842955053504b5ff04d8925591d0fb5f3
Rex Hart <75512757+rexapcs@users.noreply.github.com>
2024-04-22 15:31:51 +0200

    Upgrades

--- Assets/Scripts_AB/Upgrades/Upgrade.cs
+++ Assets/Scripts_AB/Upgrades/Upgrade.cs
@@ -1,32 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-
+using UnityEngine.Events;
 public class Upgrade : MonoBehaviour
 {
     // Start is called before the first frame update
+    public UnityEvent AttackUpgradeEvent;
+    public GameObject text;
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D collider;
     void Start()
     {
         StartCoroutine(DestroyUpgrade());
+        text.SetActive(false);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        collider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
     void OnTriggerEnter2D(Collider2D collision)
     {
         // Check if the collision is with the player
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && gameObject.CompareTag("BlueUpgrade"))
+        {
+            // Calls different actions
+            PlayerMovement movement = collision.gameObject.GetComponent<PlayerMovement>();
+            movement.UpgradeSpeed();
+            StartCoroutine(BlueShowText());
+
+        }
+        else if (collision.gameObject.CompareTag("Player") && gameObject.CompareTag("GreenUpgrade"))
         {
             // Calls different actions
             CharacterController2D player = collision.gameObject.GetComponent<CharacterController2D>();
-            PlayerMovement movement = collision.gameObject.GetComponent<PlayerMovement>();
             player.RestoreHealth();
-            movement.UpgradeSpeed();
+            StartCoroutine(GreenShowText());
+        }
+        else if (collision.gameObject.CompareTag("Player") && gameObject.CompareTag("RedUpgrade"))
+        {
+            // Calls different actions
+            AttackUpgradeEvent.Invoke();
+            Debug.Log("test");
             Destroy(gameObject);
         }
+    }
+    private IEnumerator GreenShowText()
+    {
+        text.SetActive(true);
+        spriteRenderer.enabled = false;
+        collider.enabled = false;
+        yield return new WaitForSeconds(2f);
+        text.SetActive(false);
+        Destroy(gameObject);
+
+    }
+    private IEnumerator BlueShowText()
+    {
+        text.SetActive(true);
+        spriteRenderer.enabled = false;
+        collider.enabled = false;
+        yield return new WaitForSeconds(5f);
+        text.SetActive(false);
+        Destroy(gameObject);
+
     }
     IEnumerator DestroyUpgrade()
     {
